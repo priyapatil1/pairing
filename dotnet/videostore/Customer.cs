005c6b6b4cb920d41885300b6e270a0466bd869c
Jim Suchy <jsuchy@8thlight.com>
2014-04-15 17:13:06 +0100

    Initial state for dotnet.

---
+++ dotnet/videostore/Customer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+
+namespace videostore
+{
+    class Customer
+    {
+        public Customer(String name)
+        {
+            this.name = name;
+        }
+
+        public void AddRental(Rental rental)
+        {
+            rentals.Add(rental);
+        }
+
+        public String GetName()
+        {
+            return name;
+        }
+
+        public String Statement()
+        {
+            double totalAmount = 0;
+            int frequentRenterPoints = 0;
+
+            IEnumerator rentalsEnumerator = this.rentals.GetEnumerator();
+            String result = "Rental Record for " + GetName() + "\n";
+
+            while (rentalsEnumerator.MoveNext())
+            {
+                double thisAmount = 0;
+                Rental each = (Rental) rentalsEnumerator.Current;
+
+                // determines the amount for each line
+                switch (each.GetMovie().GetPriceCode())
+                {
+                    case Movie.REGULAR:
+                        thisAmount += 2;
+                        if (each.GetDaysRented() > 2)
+                            thisAmount += (each.GetDaysRented() - 2) * 1.5;
+                        break;
+                    case Movie.NEW_RELEASE:
+                        thisAmount += each.GetDaysRented() * 3;
+                        break;
+                    case Movie.CHILDRENS:
+                        thisAmount += 1.5;
+                        if (each.GetDaysRented() > 3)
+                            thisAmount += (each.GetDaysRented() - 3) * 1.5;
+                        break;
+                }
+
+                frequentRenterPoints++;
+
+                if (each.GetMovie().GetPriceCode() == Movie.NEW_RELEASE
+                        && each.GetDaysRented() > 1)
+                    frequentRenterPoints++;
+
+                result += "\t" + each.GetMovie().GetTitle() + "\t" + string.Format("{0:F1}", thisAmount) + "\n";
+                totalAmount += thisAmount;
+
+            }
+
+            result += "You owed " + string.Format("{0:F1}", totalAmount) + "\n";
+            result += "You earned " + frequentRenterPoints + " frequent renter points\n";
+
+
+            return result;
+        }
+
+
+        private String name;
+        private ArrayList rentals = new ArrayList();
+    }
+}
---
+++ dotnet/videostore/VideoStoreTest.cs
@@ -0,0 +1,48 @@
+﻿namespace videostore
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class VideoStoreTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            customer = new Customer("Fred");
+        }
+
+        [Test]
+        public void TestSingleNewReleaseStatement()
+        {
+            customer.AddRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
+            Assert.AreEqual("Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n", customer.Statement());
+        }
+
+        [Test]
+        public void TestDualNewReleaseStatement()
+        {
+            customer.AddRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
+            customer.AddRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
+            Assert.AreEqual("Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n", customer.Statement());
+        }
+
+        [Test]
+        public void TestSingleChildrensStatement()
+        {
+            customer.AddRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3));
+            Assert.AreEqual("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n", customer.Statement());
+        }
+
+        [Test]
+        public void TestMultipleRegularStatement()
+        {
+            customer.AddRental(new Rental(new Movie("Plan 9 from Outer Space", Movie.REGULAR), 1));
+            customer.AddRental(new Rental(new Movie("8 1/2", Movie.REGULAR), 2));
+            customer.AddRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
+
+            Assert.AreEqual("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n", customer.Statement());
+        }
+
+        private Customer customer;
+    }
+}
